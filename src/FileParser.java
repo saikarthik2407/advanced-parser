d242d4b7b9fb1794da8ff45899f4d1839354c58a
Venkata Sai Karthikeya <venkatasaikarthikeya@gmail.com>
2022-10-21 09:28:33 -0700

    code refactored and updated to work for print expressions

--- src/FileParser.java
+++ src/FileParser.java
@@ -7,8 +7,8 @@ import java.util.function.Consumer;
 
 public class FileParser {
 
-    private Map<String, Integer> valueStore = new HashMap<>();
-    private List<String> codeLines = new ArrayList<>();
+    private final Map<String, Integer> valueStore = new HashMap<>();
+    private final List<String> codeLines = new ArrayList<>();
 
     FileParser() { }
 
@@ -26,7 +26,11 @@ public class FileParser {
         }
     }
 
-    public void splitCodeToSegments(BufferedReader br) {
+    /**
+     * The function splits the whole file into lines and stores those lines as list of strings
+     * @param br is the Buffered reader that has the file stored in it as String Buffer
+     */
+    private void splitCodeToSegments(BufferedReader br) {
         String line;
         try {
             while ((line = br.readLine()) != null) {
@@ -37,7 +41,10 @@ public class FileParser {
         }
     }
 
-    public void examineCodeLine() {
+    /**
+     * The function examines each line of code to parse
+     */
+    private void examineCodeLine() {
         codeLines.forEach(new Consumer<String>() {
             @Override
             public void accept(String lineOfCode) {
@@ -45,40 +52,64 @@ public class FileParser {
                 switch (codeWordsArray[0]) {
                     case "LET" -> examineInitializationStatement(codeWordsArray);
                     case "PRINT", "PRINTLN" -> examinePrintStatements(codeWordsArray);
+                    default -> System.out.println("Syntax error occurred while parsing");
                 }
             }
         });
     }
 
-    public void examineInitializationStatement(String[] codeWords) {
+    /**
+     * This function builds a numerical expression from a variable expression by retrieving the values of
+     * variables from the hashMap which we stored during initialization
+     * @param mathExpressionBuilder is the numerical expression builder
+     * @param variableExpression is the expression that has variables instead of numbers
+     * @return the numerical expression
+     */
+    private String buildMathematicalExpression(StringBuilder mathExpressionBuilder, String variableExpression) {
+        for (int i = 0; i < variableExpression.length(); i++) {
+            Boolean isContainedInMap = valueStore.containsKey(Character.toString(variableExpression.charAt(i)));
+            if (isContainedInMap) {
+                Integer x = valueStore.get(Character.toString(variableExpression.charAt(i)));
+                mathExpressionBuilder.append(x.toString());
+            } else {
+                mathExpressionBuilder.append(variableExpression.charAt(i));
+            }
+        }
+        return mathExpressionBuilder.toString();
+    }
+
+    /**
+     * This part of code handles the lines of code that deal with initialization i.e: which start with LET
+     * @param codeWords are the words of code in a particular line of code
+     */
+    private void examineInitializationStatement(String[] codeWords) {
         StringBuilder expressionBuilder = new StringBuilder();
         for (int i = 1; i < codeWords.length; expressionBuilder.append(codeWords[i++]));
         String expression = expressionBuilder.toString();
         String[] equationOperands = expression.split("=");
         String key = equationOperands[0];
         String value = equationOperands[1];
-        Boolean hasOnlyDigits = value.matches("[0-9]+");
+        boolean hasOnlyDigits = value.matches("[0-9]+");
         if (hasOnlyDigits) {
             Integer numericalValue = Integer.parseInt(equationOperands[1]);
             valueStore.put(key, numericalValue);
         } else {
             StringBuilder expressionToBeEvaluated = new StringBuilder();
-            for (int i = 0; i < value.length(); i++) {
-                Boolean isContainedInMap = valueStore.containsKey(Character.toString(value.charAt(i)));
-                if (isContainedInMap) {
-                    Integer x = valueStore.get(Character.toString(value.charAt(i)));
-                    expressionToBeEvaluated.append(x.toString());
-                } else {
-                    expressionToBeEvaluated.append(value.charAt(i));
-                }
-            }
-            int result = evaluate(expressionToBeEvaluated.toString());
+            int result = evaluate(buildMathematicalExpression(expressionToBeEvaluated, value));
             valueStore.put(key, result);
         }
     }
 
-    public void examinePrintStatements(String[] codeWords) {
-        String distinctWord = codeWords[1];
+    /**
+     * The function examines the print statements and parses them
+     * @param codeWords are words of code from print lines
+     */
+    private void examinePrintStatements(String[] codeWords) {
+        StringBuilder find = new StringBuilder();
+        for (int i = 1; i < codeWords.length; i++) {
+            find.append(codeWords[i]);
+        }
+        String distinctWord = find.toString();
         if (valueStore.containsKey(distinctWord)) {
             Integer value = valueStore.get(distinctWord);
             if (Objects.equals(codeWords[0], "PRINT"))
@@ -86,29 +117,45 @@ public class FileParser {
             else
                 System.out.println(value);
         } else {
-            StringBuilder printableString = new StringBuilder();
-            for (int i = 1; i < codeWords.length; printableString.append(codeWords[i++]));
-            String printableResult = printableString.toString().replaceAll("\"", "");
-            if (Objects.equals(codeWords[0], "PRINT"))
-                System.out.print(printableResult);
-            else
-                System.out.println(printableResult);
+            if (!distinctWord.startsWith("\"")) {
+                StringBuilder printableToBeEvaluated = new StringBuilder();
+                int expressionResult = evaluate(buildMathematicalExpression(printableToBeEvaluated, distinctWord));
+                if (Objects.equals(codeWords[0], "PRINT"))
+                    System.out.print(expressionResult);
+                else
+                    System.out.println(expressionResult);
+            } else {
+                StringBuilder printableString = new StringBuilder();
+                for (int i = 1; i < codeWords.length; printableString.append(codeWords[i++]));
+                String printableResult = printableString.toString().replaceAll("\"", "");
+                if (Objects.equals(codeWords[0], "PRINT"))
+                    System.out.print(printableResult);
+                else
+                    System.out.println(printableResult);
+            }
         }
     }
 
+    /**
+     * This code for expression evaluation has been taken from Geeks for geeks website.
+     * The function is intended to evaluate an expression containing numbers and mathematical operators
+     * Link to the article: <a href="https://www.geeksforgeeks.org/expression-evaluation/">...</a>
+     * @param expression : It is the expression that needs to be evaluated by using a stack
+     * @return : The function returns an integer i.e: the result of the evaluated expression
+     */
     public int evaluate(String expression)
     {
         char[] tokens = expression.toCharArray();
-        Stack<Integer> values = new Stack<Integer>();
-        Stack<Character> ops = new Stack<Character>();
+        Stack<Integer> values = new Stack<>();
+        Stack<Character> ops = new Stack<>();
         for (int i = 0; i < tokens.length; i++) {
             if (tokens[i] == ' ')
                 continue;
             if (tokens[i] >= '0' && tokens[i] <= '9') {
-                StringBuffer sbuf = new StringBuffer();
+                StringBuffer sBuffer = new StringBuffer();
                 while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
-                    sbuf.append(tokens[i++]);
-                values.push(Integer.parseInt(sbuf.toString()));
+                    sBuffer.append(tokens[i++]);
+                values.push(Integer.parseInt(sBuffer.toString()));
                 i--;
             }
             else if (tokens[i] == '(')
@@ -132,8 +179,7 @@ public class FileParser {
     public boolean hasPrecedence(char op1, char op2) {
         if (op2 == '(' || op2 == ')')
             return false;
-        if ((op1 == '*' || op1 == '/') &&
-                (op2 == '+' || op2 == '-'))
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
             return false;
         else
             return true;
@@ -149,9 +195,7 @@ public class FileParser {
                 return a * b;
             case '/':
                 if (b == 0)
-                    throw new
-                            UnsupportedOperationException(
-                            "Cannot divide by zero");
+                    throw new UnsupportedOperationException("Cannot divide by zero");
                 return a / b;
         }
         return 0;
