ad39e88b8ce422f16e05c9b5d48b39341d6cbdfd
Venkata Sai Karthikeya <34571463+saikarthik2407@users.noreply.github.com>
2022-11-10 17:19:24 -0800

    Gave capability to Main to parse multiple files given in command line arguments

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        // retrieve the file from the device based on the file name given in the command line arguments
-        File silFile = new File(args[0]);
-
-        // Create an instance of File Parser that supports SIL file parsing
-        FileParser silFileParser = new FileParser();
-
-        // Start SIL file parsing
-        silFileParser.parse(silFile);
+        // Parse each file given in the Command Line Arguments
+        for (String fileName: args) {
+            // retrieve the file from the device based on the file name given in the command line arguments
+            File silFile = new File(fileName);
+            // Create an instance of File Parser that supports SIL file parsing
+            FileParser silFileParser = new FileParser();
+            // Start SIL file parsing
+            silFileParser.parse(silFile);
+        }
     }
 }
